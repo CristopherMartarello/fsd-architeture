a202dccce32f7540d3eabf8bba15d68768903b4e
CristopherMartarello <crisrossi1313@gmail.com>
2026-07-09 00:21:40 -0300

    chore: setup shadcn and aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/shared/ui/button";
+
 function App() {
   return (
     <div className="min-h-screen bg-synth-base flex items-center justify-center">
-      <h1 className="text-synth-accent text-3xl font-bold">Game Tracker</h1>
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <h1 className="text-synth-accent text-3xl font-bold">Game Tracker</h1>
+        <Button className="text-synth-base cursor-pointer" variant="secondary">
+          Click Me
+        </Button>
+      </div>
     </div>
   );
 }
